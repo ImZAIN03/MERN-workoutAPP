38b17a753a8383d3da86e8fc36faefbd0602ed64
Muhammed Zain <muhammedzain2256@gmail.com>
2023-06-05 11:10:19 +0500

    end project

---
+++ backend/routes/workouts.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout
+} = require("../controllers/workcontroller")
+
+const router = express.Router();
+
+//GET all workouts
+router.get('/', getWorkouts)
+
+//GET a signal workout 
+router.get('/:id', getWorkout);
+
+//POST a new workout
+router.post('/', createWorkout)
+
+//DELETE a new workout
+router.delete('/:id',deleteWorkout)
+
+//UPDATE a new workout
+router.patch('/:id',updateWorkout)
+
+
+module.exports = router;
+
